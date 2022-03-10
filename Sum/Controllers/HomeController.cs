b54c5192dc9a0b06f63e3dac659e9b5859a1db75
dell <dell@DESKTOP-TBA7ULK>
2022-03-10 13:27:49 +0330

    Add project files.

---
+++ Sum/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sum.Models;
+using System.Diagnostics;
+
+namespace Sum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sum(int[] arr)
+        {
+            var sum = arr.Sum();
+            return Json(sum);
+        }
+    }
+}
